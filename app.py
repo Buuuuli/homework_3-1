32a44828f3f81ceeaa05dc3c7a87840746483d8e
buuuuli <km509@duke.edu>
2022-03-21 14:20:30 -0400

    add loading and matched message

--- app.py
+++ app.py
@@ -201,13 +201,16 @@ def update_candlestick_graph(n_clicks, currency_string, what_to_show,
     contract.exchange = 'IDEALPRO' # 'IDEALPRO' is the currency exchange.
     contract.currency = currency_string.split(".")[1]
 
-    contract_detail = fetch_contract_details(contract)
-    if type(contract_detail) == str:
-        return ("Error: wrong currency pairs (" + currency_string + "), please check your input"), go.Figure()
-    else:
-        s = str(contract_detail).split(",")[10]
-        if s != currency_string:
-            return ("The system currency pairs " + s +" does not match your input " + currency_string), go.Figure()
+
+
+    contract_info = fetch_contract_details(contract)
+
+    actual_pair = str(contract_info).split(",")[10]
+
+    # # # Give the candlestick figure a title
+
+
+
     ############################################################################
     ############################################################################
     # This block is the one you'll need to work on. UN-comment the code in this
@@ -244,7 +247,7 @@ def update_candlestick_graph(n_clicks, currency_string, what_to_show,
 
 
     # # # Give the candlestick figure a title
-    return ('Submitted query for ' + currency_string), fig
+    return ('Submitted query for ' + currency_string), ("Acutal Currency Pair" + actual_pair), fig
 
     # # # Give the candlestick figure a title
 
